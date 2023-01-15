d69b4fecb254903c7a224813df4bdc6e8fab3aa0
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-01-15 07:35:23 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = {};
+  let closedBracket = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    closedBracket.push(bracketsConfig[i][1]);
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  function isClosedBracket(ch) {
+    return closedBracket.indexOf(ch) > -1;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if (brackets[current] !== current) {
+      if (isClosedBracket(current)) {
+        if (brackets[current] !== stack.pop()) {
+          return false;
+        }
+      } else {
+        stack.push(current);
+      }
+    } else {
+      stack.push(current);
+      if (brackets[current] !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length == 0;
 }
